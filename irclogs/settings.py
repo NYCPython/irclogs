ebab7db3ed2971fbe02d2494dda988a9a62f8dd6
Jon Banafato <jonathan.banafato@gmail.com>
2014-03-17 17:18:21 -0400

    Changes to test database settings
    This commit moves the default testing database up to the project root (it's not
    part of the blueprint and shouldn't be stored there), adds it to the gitignore,
    and updates the documentation to reflect steps required to initially create the
    project's tables.

--- irclogs/settings.py
+++ irclogs/settings.py
@@ -1,6 +1,6 @@
 import os
 
-ROOT = os.path.abspath(os.path.dirname(__file__))
+ROOT = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(ROOT, 'dev.db'))
 ANGULAR_RESOURCE_DIRECTORY = os.path.join(ROOT, 'angular')
